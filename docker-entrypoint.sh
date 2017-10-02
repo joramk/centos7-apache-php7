494fb6260afe6515c55055603f4b7c077900407a
joramk <joramk@users.noreply.github.com>
2017-10-02 22:01:20 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,36 +2,52 @@
 set -eo pipefail
 shopt -s nullglob
 
-setup() {   
-        if [ ! -z "$HTTPD_SERVERADMIN" ]; then
-                sed -i "s/ServerAdmin root@localhost//g" /etc/httpd/conf/httpd.conf                              
-                echo "ServerAdmin $HTTPD_SERVERADMIN" >>/etc/httpd/conf/httpd.conf 
+setup() {
+        if [ ! -z "$HTTPD_REMOVE_SSL" ]; then
+                yum remove mod_ssl -y
         fi
 
-        if [ ! -z "$HTTPD_HARDENING" ]; then
+        if [ ! -z "$HTTPD_SERVERADMIN" ]; then    
+                sed -i "s/ServerAdmin root@localhost//g" /etc/httpd/conf/httpd.conf
+                echo "ServerAdmin $HTTPD_SERVERADMIN" >>/etc/httpd/conf/httpd.conf
+        fi
+
+        if [ ! -z "$HTTPD_HARDENING" ]; then    
                 sed -i 's/expose_php = On/expose_php = Off/g' /etc/opt/remi/php71/php.ini
-                sed -i 's/expose_php = On/expose_php = Off/g' /etc/php.ini
                 echo "ServerTokens Prod" >>/etc/httpd/conf/httpd.conf
                 echo "ServerSignature Off" >>/etc/httpd/conf/httpd.conf
                 echo "TraceEnable Off" >>/etc/httpd/conf/httpd.conf
         fi
 
         if [ ! -z "$PMA_FORCESSL" ]; then
-                echo "$cfg['ForceSSL'] = true;" >>/etc/phpMyAdmin/config.inc.php  
+                sed -i 's/\?>/$cfg['ForceSSL'] = true;\n\?>/g' /etc/phpMyAdmin/config.inc.php
         fi
 
-        if [ ! -z "$PMA_DBHOST" ]; then  
-                sed -i "s/localhost/$PMA_DBHOST/g" /etc/phpMyAdmin/config.inc.php               
+        if [ ! -z "$PMA_DBHOST" ]; then
+                sed -i "s/localhost/$PMA_DBHOST/g" /etc/phpMyAdmin/config.inc.php
         fi
 
-        if [ ! -z "$TIMEZONE" ]; then  
-                echo "date.timezone = $TIMEZONE" >>/etc/opt/remi/php71/php.ini                          
+        if [ ! -z "$TIMEZONE" ]; then
+                echo "date.timezone = $TIMEZONE" >>/etc/opt/remi/php71/php.ini
+                echo "date.timezone = $TIMEZONE" >>/etc/php.ini
+                ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
         fi
+}
+
+answers() {
+        echo --
+        echo SomeState
+        echo SomeCity
+        echo SomeOrganization
+        echo SomeOrganizationalUnit
+        echo localhost.localdomain
+        echo root@localhost.localdomain
 }
 
 if [ -e /firstrun ] && [ -z "$HTTPD_OMIT_FIRSTRUN" ]; then
         setup
-        rm -f /firstrun    
+        answers | /usr/bin/openssl req -newkey rsa:2048 -keyout /etc/pki/tls/private/localhost.key -nodes -x509 -days 730 -out /etc/pki/tls/certs/localhost.crt
+        rm -f /firstrun
 fi
 
 exec "$@"
